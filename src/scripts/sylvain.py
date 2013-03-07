e405ddcdd8775e785cf54a1e51fa3337ff962d90
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2013-03-07 13:28:55 +0100

    New criteria

--- src/scripts/sylvain.py
+++ src/scripts/sylvain.py
@@ -68,10 +68,15 @@ def compute_optimal_pension(e, ea, rev_smic_chef, rev_smic_part, temps_garde, uc
 
         nivvie_chef_after = df.get_value(u"chef", u"nivvie")
         nivvie_part_after = df.get_value(u"part", u"nivvie")
+        nivvie_chef_before = df.get_value(u"chef", u"nivvie")/df.get_value(u"chef", u"nivvie_loss")
+        nivvie_part_before = df.get_value(u"part", u"nivvie")/df.get_value(u"part", u"nivvie_loss")
         
 #        opt_pension = private_cost_after*(revdisp_chef + pension)/(revdisp_chef+revdisp_part) - total_cost_after_chef + public_cost_after_chef
 
         private_cost_after_chef = df.get_value(u"chef", u"prise en charge privée de l'enfant")        
+        private_cost_after_part = df.get_value(u"part", u"prise en charge privée de l'enfant")
+        private_cost_before = df.get_value(u"couple", u"prise en charge privée de l'enfant")
+        
 
         if criterium == "revdisp":
             opt_pension = private_cost_after*revdisp_chef/(revdisp_chef+revdisp_part)-private_cost_after_chef
@@ -88,6 +93,18 @@ def compute_optimal_pension(e, ea, rev_smic_chef, rev_smic_part, temps_garde, uc
             C = 1 + .3*e + .5*ea
             D = 1 + alpha*gamma*(.3*e + .5*ea)
             opt_pension = ((A-B)*revdisp_chef*revdisp_part)/((revdisp_chef/C) + (revdisp_part/D))
+        elif criterium == "share_private_cost_before_chef":
+            opt_pension = - private_cost_after_chef  + private_cost_before*nivvie_chef_before/(nivvie_chef_before+nivvie_part_before)
+        
+        elif criterium == "share_private_cost_before_part":
+            opt_pension = private_cost_after_part  - private_cost_before*nivvie_part_before/(nivvie_chef_before+nivvie_part_before)
+        
+        elif criterium == "share_private_cost_before_chef_nivvie_after":
+            opt_pension = - private_cost_after_chef  + private_cost_before*nivvie_chef_after/(nivvie_chef_after+nivvie_part_after)
+        
+        elif criterium == "share_private_cost_before_part_nivvie_after":
+            opt_pension = private_cost_after_part  - private_cost_before*nivvie_part_after/(nivvie_chef_after+nivvie_part_after)
+        
         elif criterium == "same_total_cost":
             return total_cost_after_chef+total_cost_after_part-total_cost_before
         elif criterium == "unaf":
@@ -242,7 +259,7 @@ def optimal_pension(criterium, disable_api = False):
         df = get_results_df(e, ea, rev_smic_chef, rev_smic_part, temps_garde, uc_parameters = uc_parameters, pension = 0, disabled=disabled)
     
     print df.to_string()
-    
+    return df
     
     # 3,3
     # bareme : 8863
@@ -292,7 +309,14 @@ def test():
 
 if __name__ == '__main__':
 #   compute_and_save_bareme()
-    optimal_pension("same_total_cost", disable_api=True)
+    
+#    df0 = pension_according_to_bareme(disable_api=True)
+#    df1 =  optimal_pension("same_total_cost", disable_api=True)
+#    df2 = optimal_pension("nivvie", disable_api=True)
+    optimal_pension("share_private_cost_before_part_nivvie_after", disable_api=True)
+    optimal_pension("share_private_cost_before_chef_nivvie_after", disable_api=True)
+#    for df in [df0, df1, df2]:
+#        print df.to_string()
 #    test_unaf()
 #    print get_unaf(2,0)
 #    pension_according_to_bareme()
